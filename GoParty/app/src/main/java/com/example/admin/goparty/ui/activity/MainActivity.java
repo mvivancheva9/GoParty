f05980bfbc818291bbb803c972f0220dd18ea805
Margarita Ivancheva <mvivancheva@gmail.com>
2016-04-08 14:41:40 +0300

    architecture build started

--- GoParty/app/src/main/java/com/example/admin/goparty/ui/activity/MainActivity.java
+++ GoParty/app/src/main/java/com/example/admin/goparty/ui/activity/MainActivity.java
@@ -1,13 +1,12 @@
-package com.example.admin.goparty;
+package com.example.admin.goparty.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.admin.goparty.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ GoParty/app/src/main/java/com/example/admin/goparty/ui/fragment/MainActivityFragment.java
@@ -0,0 +1,81 @@
+package com.example.admin.goparty.ui.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.admin.goparty.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    @Bind(R.id.logo_image)
+    ImageView logoImage;
+    @Bind(R.id.input_username)
+    EditText inputUsername;
+    @Bind(R.id.username_content)
+    TextInputLayout usernameContent;
+    @Bind(R.id.input_password)
+    EditText inputPassword;
+    @Bind(R.id.password_content)
+    TextInputLayout passwordContent;
+    @Bind(R.id.btn_login)
+    AppCompatButton btnLogin;
+    @Bind(R.id.link_forgotten_password)
+    TextView linkForgottenPassword;
+    Context context;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        context = inflater.getContext();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.link_forgotten_password})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                CharSequence text = "Login!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
+            case R.id.link_forgotten_password:
+                text = "Register!";
+                duration = Toast.LENGTH_SHORT;
+
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
+        }
+    }
+}
